f9e60811c8c64bcead7ce9ebad1113c85c1a1246
kayla-maharani <pkm4@teknokrat.ac.id>
2025-07-14 16:49:44 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -129,7 +129,7 @@ class ArtikelAPI:
 
 
 config_artikel = ArtikelAPI()
-config_artikel.sheet_id(1cou38HpIM8-jvKEEuL5C7PIA4mk3EIDMaSQXliBPuWE)
+config_artikel.sheet_id("1cou38HpIM8-jvKEEuL5C7PIA4mk3EIDMaSQXliBPuWE")
 config_artikel.gid('710523043')
 
 
